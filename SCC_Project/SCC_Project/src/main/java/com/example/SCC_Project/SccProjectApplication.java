4e0df2e7bc4f68a449f33d54084589a0d48ab71a
ji-cardoso <50217185+ji-cardoso@users.noreply.github.com>
2020-10-29 20:35:26 +0000

    First commit - Spring Project

---
+++ SCC_Project/SCC_Project/src/main/java/com/example/SCC_Project/SccProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.SCC_Project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SccProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SccProjectApplication.class, args);
+	}
+
+}
