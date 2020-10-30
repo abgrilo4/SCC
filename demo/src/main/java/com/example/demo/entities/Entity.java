5ab4873f915c9842b7f1e1e1080d8a82e22de4f9
abgrilo4 <ab.grilo@campus.fct.unl.pt>
2020-10-30 15:57:41 +0000

    corrections

--- demo/src/main/java/com/example/demo/entities/Entity.java
+++ demo/src/main/java/com/example/demo/entities/Entity.java
@@ -1,16 +1,26 @@
 package com.example.demo.entities;
 
+import java.util.List;
+
 import org.springframework.data.annotation.Id;
 
 import com.microsoft.azure.spring.data.cosmosdb.core.mapping.Document;
 
 @Document
 public class Entity {
-	
+
+	private static final String recommendations = "RECOMMENDATIONS";
+	private static final String frontpage = "FRONTPAGE";
+
 	@Id
 	private String id;
-	
 	private String name;
+	private String description;
+	private byte[] photo;
+	private List<String> calendarIds;
+	private int numberOfLikes;
+	private Forum forum;
+	private String listed;
 
 	public String getId() {
 		return id;
@@ -28,7 +38,36 @@ public class Entity {
 		this.name = name;
 	}
 	
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	
+	public byte[] getPhoto() {
+		return photo;
+	}
+
+	public void setPhoto(byte[] photo) {
+		this.photo = photo;
+	}
+	
+	public List<String> getCalendarIds() {
+		return calendarIds;
+	}
+
+	public void addCalendarId(String calendarId) {
+		calendarIds.add(calendarId);
+	}
 	
 	
+	
+	
+	
+
+
+
 
 }
